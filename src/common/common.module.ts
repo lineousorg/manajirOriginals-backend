1fea2852113fbf753bdfb55ef0e19caf1b39691b
Dominic009 <dpg11000@gmail.com>
2026-04-22 01:06:21 +0600

    feat(common): add PrismaModule import for service dependencies

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { PricingService } from './services/pricing.service';
 import { VariantValidationService } from './services/variant-validation.service';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PricingService, VariantValidationService],
   exports: [PricingService, VariantValidationService],
 })
